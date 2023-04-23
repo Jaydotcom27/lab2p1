9aa22064b82c49add173e95fc3ce2eb9cda08225
Jeremy Martinez <39386502+Jaydotcom27@users.noreply.github.com>
2023-04-23 14:13:58 -0400

    added static ip

--- test.sh
+++ test.sh
@@ -4,5 +4,4 @@ source ../env.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /part1/output
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /part1/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal shot_logs.csv  /part1/input/
-/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 kmeans.py hdfs://$SPARK_MASTER:9000/part1/input/
-../stop.sh
+/usr/local/spark/bin/spark-submit --master=spark://10.128.0.5:7077 kmeans.py hdfs://10.128.0.5:9000/part1/input/
